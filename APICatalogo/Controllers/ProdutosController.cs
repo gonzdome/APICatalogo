4b3f7df83b261d7e5091605584a4febd88fde556
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-01-29 22:36:33 -0300

    feat: add trycatch to ProdutosController

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -19,60 +19,100 @@ public class ProdutosController : ControllerBase
     [HttpGet]
     public ActionResult<IEnumerable<Produto>> Get()
     {
-        var response = _context.Produtos.AsNoTracking().ToList();
-        if (response is null)
-            return NotFound("Products not found!");
+        try
+        {
+            var response = _context.Produtos.AsNoTracking().ToList();
+            if (response is null)
+                return NotFound("Products not found!");
 
-        return response;
+            return response;
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "Error ocurred when calling: Get");
+        }
+
     }
 
     [HttpGet("{id:int}", Name = "GetProduct")]
     public ActionResult<Produto> GetById(int id)
     {
-        var response = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-        if (response is null)
-            return NotFound("Product not found!");
+        try
+        {
+            var response = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (response is null)
+                return NotFound("Product not found!");
 
-        return response;
+            return response;
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "Error ocurred when calling: GetById");
+        }
+
     }
 
     [HttpPost]
     public ActionResult Post(Produto product)
     {
-        if (product is null)
-            return BadRequest();
+        try
+        {
+            if (product is null)
+                return BadRequest();
 
-        _context.Produtos.Add(product);
-        _context.SaveChanges();
+            _context.Produtos.Add(product);
+            _context.SaveChanges();
 
-        return new CreatedAtRouteResult("GetProduct", new { id = product.ProdutoId }, product);
+            return new CreatedAtRouteResult("GetProduct", new { id = product.ProdutoId }, product);
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "Error ocurred when calling: Post");
+        }
+
     }
 
     [HttpPut("{id:int}")]
     public ActionResult Put(int id, Produto product)
     {
-        if(id != product.ProdutoId)
-            return BadRequest();
+        try
+        {
+            if (id != product.ProdutoId)
+                return BadRequest();
 
-        if (product is null)
-            return BadRequest();
+            if (product is null)
+                return BadRequest();
 
-        _context.Produtos.Entry(product).State = EntityState.Modified;
-        _context.SaveChanges();
+            _context.Produtos.Entry(product).State = EntityState.Modified;
+            _context.SaveChanges();
 
-        return Ok(product);
+            return Ok(product);
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "Error ocurred when calling: Put");
+        }
+
     }
 
     [HttpDelete("{id:int}")]
     public ActionResult DeleteById(int id)
     {
-        var product = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-        if (product is null)
-            return NotFound("Product not found!");
+        try
+        {
+            var product = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (product is null)
+                return NotFound("Product not found!");
 
-        _context.Produtos.Remove(product);
-        _context.SaveChanges();
+            _context.Produtos.Remove(product);
+            _context.SaveChanges();
 
-        return Ok(product);
+            return Ok(product);
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "Error ocurred when calling: DeleteById");
+        }
+
     }
 }
