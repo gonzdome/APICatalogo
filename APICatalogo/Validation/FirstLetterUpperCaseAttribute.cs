791d4071e2aa6e45d4699d28fb782c0744579e4a
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-02-05 22:40:58 -0300

    feat: add first letter upper case validation using ValidationAttribute

---
+++ APICatalogo/Validation/FirstLetterUpperCaseAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.Validation;
+
+public class FirstLetterUpperCaseAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value == null || string.IsNullOrEmpty(value.ToString()))
+            return ValidationResult.Success;
+
+        var firstLetter = value.ToString()[0].ToString();
+        if (firstLetter != firstLetter.ToString().ToUpper())
+            return new ValidationResult("The first letter must be Uppercase!");
+
+        return ValidationResult.Success;
+    }
+}
