608b7f10a9a03a756e9e629fdd05fa4d7929c769
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-02-05 22:24:26 -0300

    feat: add data annotation to validate field Nome of Categoria

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -14,11 +14,11 @@ public class Categoria
 
     [Key]
     public int CategoriaId { get; set; }
-    
-    [Required]
-    [StringLength(80)]
+
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [StringLength(20, ErrorMessage = "O nome deve ter entre 5 e 20 caracteres", MinimumLength = 5)]
     public string? Nome { get; set; }
-    
+
     [Required]
     [StringLength(300)]
     public string? ImagemUrl { get; set; }
