2cb1aafbf61e964e991880e78bc70f7a9e1a051d
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-02-05 22:24:05 -0300

    feat: add data annotation to validate field Nome/Descricao/Preco of Produto

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -10,16 +10,17 @@ public class Produto
     [Key]
     public int ProdutoId { get; set; }
 
-    [Required]
-    [StringLength(80)]
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [StringLength(20, ErrorMessage = "O nome deve ter entre 5 e 20 caracteres", MinimumLength = 5)]
     public string? Nome { get; set; }
 
     [Required]
-    [StringLength(300)]
+    [StringLength(50, ErrorMessage = "A descrição deve ter no máximo {1} caracteres")]
     public string? Descricao { get; set; }
 
     [Required]
-    [Column(TypeName="decimal(10,2)")]
+    [Column(TypeName = "decimal(10,2)")]
+    [Range(1, 10000, ErrorMessage = "O preço deve estar entre {1} e {2}")]
     public decimal Preco { get; set; }
 
     [Required]
