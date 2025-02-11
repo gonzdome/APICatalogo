64403882efa4f2ecd45beba454b98ca6be07f608
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-02-10 22:38:39 -0300

    feat: add middleware configuration on Program

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,4 +1,5 @@
 using APICatalogo.Context;
+using APICatalogo.Extensions;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 
@@ -21,6 +22,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.ConfigureExceptionHandler();
 }
 
 app.UseHttpsRedirection();
