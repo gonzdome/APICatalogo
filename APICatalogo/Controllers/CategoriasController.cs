687be080fd5f0bef2920fa34a36b652a40c2b459
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-02-12 22:13:53 -0300

    feat: add ApiLoggingFilter on GetCategories, CategoriasController

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
 ﻿using APICatalogo.Context;
+using APICatalogo.Filters;
 using APICatalogo.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +18,7 @@ public class CategoriasController : ControllerBase
     }
 
     [HttpGet("GetCategories")]
+    [ServiceFilter(typeof(ApiLoggingFilter))]
     public async Task<ActionResult<IEnumerable<Categoria>>> GetCategories()
     {
         try
