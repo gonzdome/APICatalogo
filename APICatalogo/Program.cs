0b660d6cdc487afad2ae73dfd621cf6051fb1dd8
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-02-16 23:41:33 -0300

    feat: add ApiExceptionFilter configuration on Program

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -8,6 +8,14 @@ using System.Text.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(typeof(ApiExceptionFilter));
+})
+.AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 
 builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
