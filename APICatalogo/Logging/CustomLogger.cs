793f02f6c1b49f92d0e183cdde0e863fb62e3d5d
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-02-16 23:14:25 -0300

    feat: add CustomLogger classes

---
+++ APICatalogo/Logging/CustomLogger.cs
@@ -0,0 +1,51 @@
+﻿namespace APICatalogo.Logging;
+
+public class CustomLogger : ILogger
+{
+    readonly string loggerName;
+    readonly CustomLoggerProviderConfiguration loggerConfig;
+
+    public CustomLogger(string name, CustomLoggerProviderConfiguration config)
+    {
+        loggerName = name;
+        loggerConfig = config;
+    }
+
+    public IDisposable BeginScope<TState>(TState state)
+    {
+        return null;
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return logLevel == loggerConfig.LogLevel;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+    {
+        string message = $"{logLevel.ToString()}: {eventId.Id} - {formatter(state, exception)}";
+
+        WriteTextOnFile(message);
+    }
+
+    private void WriteTextOnFile(string message)
+    {
+        string directoryPath = @"C:\ApiCatalogoLogs";
+        if (!Directory.Exists(directoryPath))
+            Directory.CreateDirectory(directoryPath);
+
+        string filePath = @$"{directoryPath}\Log_{DateTime.Now.Ticks}";
+        using (StreamWriter streamWriter = new StreamWriter(filePath, true))
+        {
+            try
+            {
+                streamWriter.WriteLine(message);
+                streamWriter.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
