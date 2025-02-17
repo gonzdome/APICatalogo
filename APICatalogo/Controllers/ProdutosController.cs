1f5bdc6599d58cb07dc14b33aefe9dc43753f8db
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-02-16 23:42:05 -0300

    refactor: remove try/catches from ProdutosController

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -19,101 +19,61 @@ public class ProdutosController : ControllerBase
     [HttpGet("GetProducts")]
     public async Task<ActionResult<IEnumerable<Produto>>> GetProducts()
     {
-        try
-        {
-            var response = await _context.Produtos.AsNoTracking().ToListAsync();
-            if (response is null)
-                return NotFound("Products not found!");
+        var response = await _context.Produtos.AsNoTracking().ToListAsync();
+        if (response is null)
+            return NotFound("Products not found!");
 
-            return response;
-        }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, "Error ocurred when calling: GetProducts");
-        }
-
+        return response;
     }
 
     [HttpGet("{id:int}", Name = "GetProductDetailsById")]
     public async Task<ActionResult<Produto>> GetProductDetailsById(int id)
     {
-        try
-        {
-            var response = await _context.Produtos.FirstOrDefaultAsync(p => p.ProdutoId == id);
-            if (response is null)
-                return NotFound("Product not found!");
+        var response = await _context.Produtos.FirstOrDefaultAsync(p => p.ProdutoId == id);
+        if (response is null)
+            return NotFound("Product not found!");
 
-            return response;
-        }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, "Error ocurred when calling: GetProductDetailsById");
-        }
-
+        return response;
     }
 
     [HttpPost]
     [Route("CreateProduct")]
     public ActionResult<Produto> CreateProduct(Produto product)
     {
-        try
-        {
-            if (product is null)
-                return BadRequest();
+        if (product is null)
+            return BadRequest();
 
-            _context.Produtos.Add(product);
-            _context.SaveChanges();
+        _context.Produtos.Add(product);
+        _context.SaveChanges();
 
-            return new CreatedAtRouteResult("GetProductDetailsById", new { id = product.ProdutoId }, product);
-        }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, "Error ocurred when calling: CreateProduct");
-        }
-
+        return new CreatedAtRouteResult("GetProductDetailsById", new { id = product.ProdutoId }, product);
     }
 
     [HttpPut("{id:int}", Name = "UpdateProductById")]
     public ActionResult<Produto> UpdateProductById(int id, Produto product)
     {
-        try
-        {
-            if (id != product.ProdutoId)
-                return BadRequest();
+        if (id != product.ProdutoId)
+            return BadRequest();
 
-            if (product is null)
-                return BadRequest();
+        if (product is null)
+            return BadRequest();
 
-            _context.Produtos.Entry(product).State = EntityState.Modified;
-            _context.SaveChanges();
+        _context.Produtos.Entry(product).State = EntityState.Modified;
+        _context.SaveChanges();
 
-            return Ok(product);
-        }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, "Error ocurred when calling: UpdateProductById");
-        }
-
+        return Ok(product);
     }
 
     [HttpDelete("{id:int}", Name = "DeleteProductById")]
     public ActionResult<Produto> DeleteProductById(int id)
     {
-        try
-        {
-            var product = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-            if (product is null)
-                return NotFound("Product not found!");
+        var product = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+        if (product is null)
+            return NotFound("Product not found!");
 
-            _context.Produtos.Remove(product);
-            _context.SaveChanges();
+        _context.Produtos.Remove(product);
+        _context.SaveChanges();
 
-            return Ok(product);
-        }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, "Error ocurred when calling: DeleteProductById");
-        }
-
+        return Ok(product);
     }
 }
