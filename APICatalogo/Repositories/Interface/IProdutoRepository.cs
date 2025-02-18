c6a00278fe6415c31817754dd58add23927db7ba
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-02-17 23:52:49 -0300

    feat: add IProdutoRepository

---
+++ APICatalogo/Repositories/Interface/IProdutoRepository.cs
@@ -0,0 +1,12 @@
+﻿using APICatalogo.Models;
+
+namespace APICatalogo.Repositories.Interface;
+
+public interface IProdutoRepository
+{
+    IEnumerable<Produto> GetProducts();
+    Produto GetProductDetailsById(int id);
+    Produto CreateProduct(Produto product);
+    Produto UpdateProductById(int id, Produto product);
+    Produto DeleteProductById(int id);
+}
