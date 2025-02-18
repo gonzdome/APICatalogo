60267e65b247df2f595cfa5918bec693a2fe8611
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-02-17 23:52:37 -0300

    feat: add ICategoriaRepository

---
+++ APICatalogo/Repositories/Interface/ICategoriaRepository.cs
@@ -0,0 +1,13 @@
+﻿using APICatalogo.Models;
+
+namespace APICatalogo.Repositories.Interface;
+
+public interface ICategoriaRepository
+{
+    IEnumerable<Categoria> GetCategories();
+    IEnumerable<Categoria> GetProductCategories();
+    Categoria GetCategoryDetailsById(int id);
+    Categoria CreateCategory(Categoria categoria);
+    Categoria UpdateCategoryById(int id, Categoria categoria);
+    Categoria DeleteCategoryById(int id);
+}
