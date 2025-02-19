ec8ac28df256efb2b8abaa5183414339f01ac85f
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-02-18 23:50:10 -0300

    feat: add logic on ProdutoRepository

---
+++ APICatalogo/Repositories/ProdutoRepository.cs
@@ -0,0 +1,61 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using APICatalogo.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Repositories;
+
+public class ProdutoRepository : IProdutoRepository
+{
+    private readonly AppDbContext _context;
+
+    public ProdutoRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public Produto CreateProduct(Produto produto)
+    {
+        if (produto is null)
+            throw new ArgumentNullException(nameof(produto));
+
+        _context.Produtos.Add(produto);
+        _context.SaveChanges();
+
+        return produto;
+    }
+
+    public Produto DeleteProductById(int id)
+    {
+        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+        if (produto is null)
+            throw new ArgumentNullException(nameof(produto));
+
+        _context.Produtos.Remove(produto);
+        _context.SaveChanges();
+
+        return produto;
+    }
+
+    public Produto GetProductDetailsById(int id)
+    {
+        return _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+    }
+
+    public IEnumerable<Produto> GetProducts()
+    {
+        return _context.Produtos.AsNoTracking().ToList();
+    }
+
+    public Produto UpdateProductById(int id, Produto product)
+    {
+        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+        if (produto is null)
+            throw new ArgumentNullException(nameof(produto));
+
+        _context.Produtos.Update(produto);
+        _context.SaveChanges();
+
+        return produto;
+    }
+}
