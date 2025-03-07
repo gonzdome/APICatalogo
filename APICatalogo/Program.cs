2bf95996d71bc754d451ab803d6a5748f4cb0ece
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-06 21:21:44 -0300

    feat: add scope for GenericRepository on Program

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -30,6 +30,7 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(mySqlCon
 builder.Services.AddScoped<ApiLoggingFilter>();
 builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
 builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 builder.Logging.AddProvider(new CustomLoggerProvider(new CustomLoggerProviderConfiguration
 {
