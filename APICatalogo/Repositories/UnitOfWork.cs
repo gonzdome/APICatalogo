1b569a68620fe36b35e29be1103ef1ba706c8e69
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-18 19:22:47 -0300

    feat: adding lazy loading for repositories and methods Commit/Dispose for context calls

--- APICatalogo/Repositories/UnitOfWork.cs
+++ APICatalogo/Repositories/UnitOfWork.cs
@@ -1,5 +1,37 @@
-﻿namespace APICatalogo.Repositories;
+﻿using APICatalogo.Context;
+using APICatalogo.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
-public class UnitOfWork
+namespace APICatalogo.Repositories;
+
+public class UnitOfWork : IUnitOfWork
 {
+    private IProdutoRepository? _produtoRepository;
+    private ICategoriaRepository? _categoriaRepository;
+    public AppDbContext _context;
+
+    public UnitOfWork (AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IProdutoRepository ProdutoRepository // Lazy Loading: ensures that the repository will only be instantiated if there is no instance of it
+    {
+        get { return _produtoRepository = _produtoRepository ?? new ProdutoRepository(_context); }
+    }
+
+    public ICategoriaRepository CategoriaRepository
+    {
+        get { return _categoriaRepository = _categoriaRepository ?? new CategoriaRepository(_context); }
+    }
+
+    public void Commit()
+    {
+        _context.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
 }
