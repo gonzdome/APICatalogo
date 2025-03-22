6df82d70a7ba2fa1ffd76090773f85f376cf5b8c
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-22 17:28:43 -0300

    feat: creating CategoriaDTO

---
+++ APICatalogo/DTOs/CategoriaDTO.cs
@@ -0,0 +1,18 @@
+﻿using APICatalogo.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.DTOs;
+
+public class CategoriaDTO
+{
+    public int CategoriaId { get; set; }
+
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [StringLength(20, ErrorMessage = "O nome deve ter entre 5 e 20 caracteres", MinimumLength = 5)]
+    [FirstLetterUpperCase]
+    public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
+    public string? ImagemUrl { get; set; }
+}
