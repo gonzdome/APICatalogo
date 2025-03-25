b35c7e4d492c79e29edb70b3f4c2d3b2fa2e62db
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-24 22:38:05 -0300

    feat: create mapping extension to Category, CategoriaDTOMappingExtensions

---
+++ APICatalogo/DTOs/Mappings/CategoriaDTOMappingExtensions.cs
@@ -0,0 +1,32 @@
+﻿using APICatalogo.Models;
+
+namespace APICatalogo.DTOs.Mappings;
+
+public static class CategoriaDTOMappingExtensions
+{
+    public static CategoriaDTO MapToCategoryDTO(this Categoria category)
+    {
+        if (category == null)
+            return null;
+
+        return new CategoriaDTO()
+        {
+            CategoriaId = category.CategoriaId,
+            Nome = category.Nome,
+            ImagemUrl = category.ImagemUrl
+        };
+    }
+
+    public static Categoria MapToCategory(this CategoriaDTO categoryDTO)
+    {
+        if (categoryDTO == null)
+            return null;
+
+        return new Categoria()
+        {
+            CategoriaId = categoryDTO.CategoriaId,
+            Nome = categoryDTO.Nome,
+            ImagemUrl = categoryDTO.ImagemUrl
+        };
+    }
+}
