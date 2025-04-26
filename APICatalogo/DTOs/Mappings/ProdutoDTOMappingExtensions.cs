70ca8ef5a08c98e02ae66c6fd5bfa6dbb3b8a638
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-25 23:28:16 -0300

    feat: add ProductDTO and Mapping Extentions

---
+++ APICatalogo/DTOs/Mappings/ProdutoDTOMappingExtensions.cs
@@ -0,0 +1,42 @@
+﻿namespace APICatalogo.DTOs.Mappings;
+
+public static class ProdutoDTOMappingExtensions
+{
+    public static ProdutoDTO MapToCategoryDTO(this Produto product)
+    {
+        if (product == null)
+            return null;
+
+        return new ProdutoDTO()
+        {
+            ProdutoId = product.ProdutoId,
+            Nome = product.Nome,
+            Descricao = product.Descricao,
+            Preco = product.Preco,
+            ImagemUrl = product.ImagemUrl,
+            Estoque = product.Estoque,
+            DataCadastro = product.DataCadastro,
+            CategoriaId = product.CategoriaId,
+            Categoria = product.Categoria,
+        };
+    }
+
+    public static Produto MapToCategory(this ProdutoDTO productDTO)
+    {
+        if (productDTO == null)
+            return null;
+
+        return new Produto()
+        {
+            ProdutoId = productDTO.ProdutoId,
+            Nome = productDTO.Nome,
+            Descricao = productDTO.Descricao,
+            Preco = productDTO.Preco,
+            ImagemUrl = productDTO.ImagemUrl,
+            Estoque = productDTO.Estoque,
+            DataCadastro = productDTO.DataCadastro,
+            CategoriaId = productDTO.CategoriaId,
+            Categoria = productDTO.Categoria,
+        };
+    }
+}
