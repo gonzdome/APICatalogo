ed5d17f95fe18c7468928763fe6d225a7389d609
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-26 23:07:00 -0300

    feat: add GlobalUsing

--- APICatalogo/GlobalUsing.cs
+++ APICatalogo/GlobalUsing.cs
@@ -1,5 +1,14 @@
-﻿global using APICatalogo.Helpers;
+﻿global using APICatalogo.DTOs;
 global using APICatalogo.Models;
+global using APICatalogo.Context;
+global using APICatalogo.Validation;
+global using APICatalogo.Helpers.Paginate;
 global using APICatalogo.Repositories.Interfaces;
 global using APICatalogo.Services.Interfaces;
 global using Microsoft.AspNetCore.Mvc;
+
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
+global using System.Collections.ObjectModel;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
