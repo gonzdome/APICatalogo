624613e83330aae3a0dde766e3d8216beba3aca8
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-28 22:13:26 -0300

    refactor: removing unnused method from ProdutoRepository (old get)

--- APICatalogo/Repositories/ProdutoRepository.cs
+++ APICatalogo/Repositories/ProdutoRepository.cs
@@ -11,18 +11,10 @@ public class ProdutoRepository : GenericRepository<Produto>, IProdutoRepository
         return GetAll().Where(c => c.CategoriaId == categoryId);
     }
 
-    //public IEnumerable<Produto> GetPaginatedProducts(Pagination pagination)
-    //{
-    //    return GetAll().OrderBy(p => p.Nome)
-    //                   .Skip((pagination.PageNumber - 1) * pagination.PageSize)
-    //                   .Take(pagination.PageSize).ToList();
-    //}
-
     public PagedList<Produto> GetPaginatedProducts(Pagination pagination)
     {
         var products = GetAll().OrderBy(p => p.Nome).AsQueryable();
         var paginatedProducts = PagedList<Produto>.ToPagedList(products, pagination.PageNumber, pagination.PageSize);
         return paginatedProducts;
     }
-
 }
