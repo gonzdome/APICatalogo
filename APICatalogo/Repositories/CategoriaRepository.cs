b4c6542840b60cf8d367a4f29eb8a8cc3bdf07d6
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-28 22:12:36 -0300

    feat: add GetPaginatedCategories on CategoriaRepository and Interface

--- APICatalogo/Repositories/CategoriaRepository.cs
+++ APICatalogo/Repositories/CategoriaRepository.cs
@@ -11,6 +11,13 @@ public class CategoriaRepository : GenericRepository<Categoria>, ICategoriaRepos
     {
     }
 
+    public PagedList<Categoria> GetPaginatedCategories(Pagination pagination)
+    {
+        var categories = GetAll().OrderBy(p => p.Nome).AsQueryable();
+        var paginatedCategories = PagedList<Categoria>.ToPagedList(categories, pagination.PageNumber, pagination.PageSize);
+        return paginatedCategories;
+    }
+
     public IEnumerable<Categoria> GetProductCategories()
     {
         return _context.Categorias.Include(c => c.Produtos).AsNoTracking().ToList();
--- APICatalogo/Repositories/Interfaces/ICategoriaRepository.cs
+++ APICatalogo/Repositories/Interfaces/ICategoriaRepository.cs
@@ -4,5 +4,6 @@ namespace APICatalogo.Repositories.Interfaces;
 
 public interface ICategoriaRepository : IGenericRepository<Categoria>
 {
+    PagedList<Categoria> GetPaginatedCategories(Pagination pagination);
     IEnumerable<Categoria> GetProductCategories();
 }
