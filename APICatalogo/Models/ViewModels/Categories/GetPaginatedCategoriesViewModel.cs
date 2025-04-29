0456fa734ad3d20942e802483033654ce6c0c1ad
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-28 23:09:54 -0300

    feat: add GetPaginatedCategoriesViewModel

---
+++ APICatalogo/Models/ViewModels/Categories/GetPaginatedCategoriesViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace APICatalogo.Models.ViewModels.Categories;
+
+public class GetPaginatedCategoriesViewModel
+{
+    public IEnumerable<CategoriaDTO> categories { get; set; }
+    public PaginationMetadata paginationMetadata { get; set; }
+}
