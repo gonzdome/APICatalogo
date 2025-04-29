8e789146c1c3dd38e73ef4203660f5cc97324260
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-28 23:10:05 -0300

    feat: add GetPaginatedProductsViewModel

---
+++ APICatalogo/Models/ViewModels/Products/GetPaginatedProductsViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace APICatalogo.Models.ViewModels.Products;
+
+public class GetPaginatedProductsViewModel
+{
+    public IEnumerable<ProdutoDTO> products { get; set; }
+    public PaginationMetadata paginationMetadata { get; set; }
+}
