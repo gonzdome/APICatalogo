22c63ee6444efd1cdb00afb605138f5ac5c5dffe
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-02 19:01:35 -0300

    feat: add ProductPriceSearch to filter Products

---
+++ APICatalogo/Helpers/Paginate/Filters/ProductPriceSearch.cs
@@ -0,0 +1,7 @@
+﻿namespace APICatalogo.Helpers.Paginate.Filters;
+
+public class ProductPriceSearch : Pagination
+{
+    public decimal? Price { get; set; }
+    public string? Condition { get; set; }
+}
