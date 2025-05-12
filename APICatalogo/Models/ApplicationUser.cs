8329d90e2dc5d48e5b58e196ca8da852244a6a37
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-12 17:17:50 -0300

    feat: add ApplicationUser model and it's migrations

---
+++ APICatalogo/Models/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿namespace APICatalogo.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public string? RefreshToken { get; set; }
+    public DateTime RefreshTokenExpiryTime { get; set; }
+}
