fe742e6b4ffc3e870bf2c1ef2352e49ef054e2b3
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-12 17:26:17 -0300

    feat: add new GlobalUsings

--- APICatalogo/GlobalUsing.cs
+++ APICatalogo/GlobalUsing.cs
@@ -1,11 +1,19 @@
 ﻿global using Newtonsoft.Json;
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 global using APICatalogo.DTOs;
 global using APICatalogo.Models;
 global using APICatalogo.Filters;
 global using APICatalogo.Context;
+global using APICatalogo.Logging;
+global using APICatalogo.Services;
 global using APICatalogo.Validation;
+global using APICatalogo.Extensions;
+global using APICatalogo.Repositories;
 global using APICatalogo.Helpers.Paginate;
 global using APICatalogo.Helpers.Paginate.Filters;
 global using APICatalogo.Repositories.Interfaces;
@@ -15,6 +23,7 @@ global using APICatalogo.Models.ViewModels;
 global using APICatalogo.Models.ViewModels.Categories;
 global using APICatalogo.Models.ViewModels.Products;
 
+global using System.Text;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
 global using System.Collections.ObjectModel;
